cf0c194711ed31620936526eec0d25f968f61ab5
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-12-14 11:22:09 -0800

    Set up server

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
+const express = require('express');
+const CORS = require('cors');
 const admin = require('firebase-admin');
+const server = express();
+
 const app = admin.initializeApp();
+
 
 app({
     credential: admin.credential.applicationDefault(),
     databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
   });
+
+
+server.use(CORS());
+
+server.get('/', (req, res) => {
+    //gives all the recipes
+})
+
+server.get('/:id', (req, res) => {
+    //gets recipe by id
+})
+
+server.post('/recipe', (req, res) => {
+    //adds new recipe
+})
+
+const port = //environment or 3200
+
+server.listen(port, () => {
+    console.log(`Server is listening on ${port}`)
+})
